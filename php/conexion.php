dcfd8d3bde7c7b998a028785cab3cf0e319521f9
Ariadna Perez <ariadna.perez97@yahoo.com>
2020-03-23 15:08:09 -0600

    Conexion php

---
+++ php/conexion.php
@@ -0,0 +1,14 @@
+<?php
+  $serverName= 'localhost';
+  $connectionInfo=array ("Database"=>"Empleados","UID"=>"sa","PWD"=>"asd.4567","CharacterSet"=>"UTF-8");
+  $conn = sqlsrv_connect($serverName,$connectionInfo); 
+
+   if($conn){
+      echo "conexion exitosa";
+   }
+   else{
+       echo "fallo en la conexion";
+       die (print_r(sqlsrv_errors(),true));
+   }
+
+?>
