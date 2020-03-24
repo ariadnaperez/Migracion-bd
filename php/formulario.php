ec8ce205e1a39dfbd4a66fe051306823628ae3ef
Alejandra <inessanchez_1996@hotmail,es>
2020-03-23 18:40:31 -0600

    Bootstrap+ventanamodal

--- php/formulario.php
+++ php/formulario.php
@@ -1,73 +1,56 @@
-<form action="extraer.php" method="get">
-    <p>Bases de Datos Existentes: </p>
-    
-<select name="ad" onchange="salta(this.form)">
-    <option selected> Seleccione una Base de Datos
-    <option value="xxxx.htm">texto 1
-    <option value="yyyy.htm">texto 2
-    <option value="zzzz.htm">texto 3
-</select>
- <br></br>
- <p>Tablas existentes en la Base de Datos</p>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+<script src="jquery-3.1.1.min.js"></script>
+<script src="bootstrap/js/bootstrap.js"></script>
+	<title>modal</title>
+</head>
+<body>
+		<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#mimodal">
+ Ver Tipo de Datos
+</button>
 
-<select name="ad" onchange="salta(this.form)">
-    <option selected> Seleccione una Tabla para visulizar las columnas 
-    <option value="xxxx.htm">texto 1
-    <option value="yyyy.htm">texto 2
-    <option value="zzzz.htm">texto 3
-</select>
-
-<br></br>
-
-   <!-- Trigger the modal with a button -->
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-
-    <!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
-
-    <!-- Modal content-->
+<!-- Modal -->
+<div class="modal fade" id="mimodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
+      <h4 class="modal-title" id="myModalLabel">Tipos de Datos</h4>
+
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
       </div>
       <div class="modal-body">
-        <p>Some text in the modal.</p>
+      <table class="table">
+  <thead>
+    <tr>
+      
+      <th scope="col">SQL SERVER</th>
+      <th scope="col">ORACLE </th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Dato1</td>
+      <td>Dato1</td>
+    </tr>
+    <tr>
+      <td>Dato2</td>
+      <td>Dato2</td>
+      
+    </tr>
+  
+  </tbody>
+</table>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-default" data-dismiss="modal">Migrar BD</button>
       </div>
     </div>
-
   </div>
 </div>
 
-
-<br></br>
-<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Migrar Base de Datos</button>
-
-   <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
-
-<div id="myModal" class="modal fade" role="dialog">
-<div class="modal-dialog">
-
-<!-- Modal content-->
-<div class="modal-content">
-  <div class="modal-header">
-    <button type="button" class="close" data-dismiss="modal">&times;</button>
-    <h4 class="modal-title">Modal Header</h4>
-  </div>
-  <div class="modal-body">
-    <p>Some text in the modal.</p>
-  </div>
-  <div class="modal-footer">
-    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-  </div>
-</div>
-
-</div>
-</div>
-
-
-  </form>
+</body>
+</html>
